b781f23d474a5a46c19e5f521b87f0270e349377
cole sobel <colesobel@coles-MacBook-Air.local>
2016-05-31 22:26:23 -0600

    videos embedded

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 
     $('#submit').click(function() {
 
-        $('.results').empty()
-        $('.results').addClass('result-active')
-        $('.results').accordion()
+        $('.results-left').empty()
+        $('.results-right').empty()
+        $('.error').empty()
+        $('#guide').slideUp(500)
         var song = $('#song').val().trim()
         var artist = $('#artist').val().trim()
         var apiKey = 'd78ab56ad21c652f6fcaed4ae1d11a2a'
@@ -14,28 +15,31 @@ $(document).ready(function() {
             type: 'GET',
             url: `http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${artist}&track=${song}&api_key=${apiKey}&format=json`,
             success: function(data) {
-                var flag = true
                 if (data.message === 'Track not found' || data.similartracks.track.length === 0) {
-                    $('.results').append('<h2 class="error">Sorry, no track information found</h2>')
+                    $('.error').html('Sorry, no track information found').show(500)
                 } else {
+                    var flag = true
                     data.similartracks.track.forEach(function(track) {
                         if (flag === true) {
-                            $('.results').append(`<div class="result-tab left"><img src="${track.image[0]['#text']}"/><p>${track.name}, ${track.artist.name}</p></div>`)
+                            $('.results-left').append(`<div class="result-tab left" data-track="${track.name}" data-artist="${track.artist.name}"><img src="${track.image[0]['#text']}"/><p>${track.name}, ${track.artist.name}</p></div>`)
                             flag = !flag
-                            console.log(flag);
+                            // console.log(flag);
                         } else {
-                            $('.results').append(`<div class="result-tab right"><img src="${track.image[0]['#text']}"/><p>${track.name}, ${track.artist.name}</p></div>`)
+                            $('.results-right').append(`<div class="result-tab right" data-track="${track.name}" data-artist="${track.artist.name}"><img src="${track.image[0]['#text']}"/><p>${track.name}, ${track.artist.name}</p></div>`)
                             flag = !flag
-                            console.log(flag);
+                            // console.log(flag);
                         }
                     })
                 }
 
 
                 $('.result-tab').click(function() {
-                    $(this).animate({
-                        height: '200px'
-                    })
+                    $(this).toggleClass('clicked-tab', 500)
+                    // console.log($(this).attr('data-track'));
+                    // console.log($(this).attr('data-artist'));
+                    // $('.clicked-tab').append('<p class="clicked-tab">Hello!</p>')
+                    // .remove('.track-info')
+                    $('.clicked-tab').not(this).removeClass('clicked-tab', 500)
                 });
 
 
